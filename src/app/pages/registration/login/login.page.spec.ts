7f1809ac5ffe7c24d49639b8db458e83390b4364
RichardRomero <ric.romero@duocuc.cl>
2022-12-14 11:18:07 -0300

    prueba unitaria

--- src/app/pages/registration/login/login.page.spec.ts
+++ src/app/pages/registration/login/login.page.spec.ts
@@ -1,9 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { NavigationExtras, Router } from '@angular/router';
+import { StorageService } from 'src/app/services/storage.service';
 
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule
+} from '@angular/forms';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, NgModule, OnInit, Output } from '@angular/core';
 import { LoginPage } from './login.page';
+import { Usuario } from '../../../interfaces/users';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-describe('LoginPage', () => {
+xdescribe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
@@ -22,3 +36,52 @@ describe('LoginPage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+
+describe('mi Storagea', () => {
+
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+
+  beforeEach( () => {
+    TestBed.configureTestingModule({
+      imports:[ReactiveFormsModule,HttpClientModule],
+      declarations:[LoginPage],
+      providers: [Storage,StorageService]
+    });
+
+    fixture = TestBed.createComponent(LoginPage);
+
+    component = fixture.componentInstance;
+    component.ngOnInit();    
+
+  });
+  
+  it('form inválida cuando está vacía', () => {
+    expect(component.frmlogin.valid).toBeFalsy();
+  });
+  
+
+
+
+});
+//describe('AppComponent', () => {
+
+  //beforeEach(waitForAsync(() => {
+
+//    TestBed.configureTestingModule({
+////     declarations: [LoginPage],
+//      imports:[FormBuilder,FormGroup,FormControl,Validators],
+//      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+//    }).compileComponents();
+//  }));
+
+//  it('should create the app', () => {
+//    const fixture = TestBed.createComponent(LoginPage);
+//    const app = fixture.debugElement.componentInstance;
+//    expect(app).toBeTruthy();
+//  });
+  // TODO: add more tests!
+
+//});
